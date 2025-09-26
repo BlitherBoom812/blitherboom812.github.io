bfcc1a266f7780dc0aeaa3885c5a01854fcf920d
Guoyun Tian <guoyun812@qq.com>
2025-09-26 19:31:25 +0800

    test bug

--- scripts/katex-patch.js
+++ scripts/katex-patch.js
@@ -1,21 +1,9 @@
 //目标：渲染多行的公式
-function process_katex_formula(input) {
-    processed_input = input
-    processed_input = processed_input.replace("\\{", "\\lbrace").replace("\\}", "\\rbrace").replace("<", "< ")
-    return processed_input
-}
-
 debug = require("./config").debug;
 module.exports = function (data) {
     console.log("Katex patch loaded. Made by BlitherBoom812.");
-    // data.content = data.content.replace(/\$(([^\n]| )+?)\$/g, (match, p1, offset, string) => {
-    //     return `$${process_katex_formula(p1).replace("_", "\\_")}$`
-    // });
-    // data.content = data.content.replace(/\$\$((.|\n)*?)\$\$/g, (match, p1, offset, string) => {
-    //     console.log("matched $$: ", p1)
-    //     replaced_content = `<div>$$ ${process_katex_formula(p1)} $$</div>`
-    //     console.log("replaced_content: ", replaced_content)
-    //     return replaced_content
-    // });
+    data.content = data.content.replace(/\$\$((.|\n)*?)\$\$/g, "<div>$$$$$1$$$$<\/div>");
+    data.content = data.content.replace("\\{", "\\lbrace");
+    data.content = data.content.replace("\\}", "\\rbrace");
     return data;
 };
