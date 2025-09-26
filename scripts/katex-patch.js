5cda2d75dafc8e10555eb0f221954b39dbc52ec0
Guoyun Tian <guoyun812@qq.com>
2025-09-26 20:13:16 +0800

    test katex patch

--- scripts/katex-patch.js
+++ scripts/katex-patch.js
@@ -2,22 +2,25 @@
 debug = require("./config").debug;
 function process_katex_formula(input) {
     processed_input = input
-    processed_input = processed_input.replace("\\{", "\\lbrace").replace("\\}", "\\rbrace").replace("<", "< ")
+    processed_input = processed_input
+        .replace("\\{", "\\lbrace")
+        .replace("\\}", "\\rbrace") //没这个会报错
+        .replace("<", "< ") // 防止转义成 html 标签
     return processed_input
 }
 module.exports = function (data) {
     console.log("Katex patch loaded. Made by BlitherBoom812.");
-    
+    console.log(data.content)
     data.content = data.content.replace(/\$([^\n$]+)\$/g, (match, p1) => {
-        return `$${process_katex_formula(p1).replace(/_/g, "\\_")}$`
+        console.log("matched: ", match)
+        return_value = process_katex_formula(p1)
+            .replace(/_/g, "\\_") // 防止被解析成 markdown 的下划线
+            .replace("\\\\", "\\\\\\\\") // 防止被解析成 markdown 的转义符
+        // console.log("revised: ", return_value)
+        return `$${return_value}$`
     });
     data.content = data.content.replace(/\$\$([\s\S]*?)\$\$/g, (match, p1) => {
         return `<div>${process_katex_formula(match)}</div>`
     });
-    // data.content = data.content.replace(/\$\$((.|\n)*?)\$\$/g, (match, p1, offset, string) => {
-    //     return `<div>$$ ${p1} $$</div>`
-    // });
-    data.content = data.content.replace("\\{", "\\lbrace");
-    data.content = data.content.replace("\\}", "\\rbrace");
     return data;
 };
