f068bcc78a84cc948b5acfe0125411f360721996
Guoyun Tian <guoyun812@qq.com>
2025-09-26 18:52:30 +0800

    20250926

--- scripts/katex-patch.js
+++ scripts/katex-patch.js
@@ -8,14 +8,14 @@ function process_katex_formula(input) {
 debug = require("./config").debug;
 module.exports = function (data) {
     console.log("Katex patch loaded. Made by BlitherBoom812.");
-    data.content = data.content.replace(/\$(([^\n]| )+?)\$/g, (match, p1, offset, string) => {
-        return `$${process_katex_formula(p1).replace("_", "\\_")}$`
-    });
-    data.content = data.content.replace(/\$\$((.|\n)*?)\$\$/g, (match, p1, offset, string) => {
-        console.log("matched $$: ", p1)
-        replaced_content = `<div>$$ ${process_katex_formula(p1)} $$</div>`
-        console.log("replaced_content: ", replaced_content)
-        return replaced_content
-    });
+    // data.content = data.content.replace(/\$(([^\n]| )+?)\$/g, (match, p1, offset, string) => {
+    //     return `$${process_katex_formula(p1).replace("_", "\\_")}$`
+    // });
+    // data.content = data.content.replace(/\$\$((.|\n)*?)\$\$/g, (match, p1, offset, string) => {
+    //     console.log("matched $$: ", p1)
+    //     replaced_content = `<div>$$ ${process_katex_formula(p1)} $$</div>`
+    //     console.log("replaced_content: ", replaced_content)
+    //     return replaced_content
+    // });
     return data;
 };
