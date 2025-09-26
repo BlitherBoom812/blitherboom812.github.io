ca42e3b9e5b8b11cfc8d2c5ea2ed82c0401642b4
Guoyun Tian <guoyun812@qq.com>
2025-09-26 17:21:50 +0800

    update katex

--- scripts/katex-patch.js
+++ scripts/katex-patch.js
@@ -1,21 +1,21 @@
 //目标：渲染多行的公式
+function process_katex_formula(input) {
+    processed_input = input
+    processed_input = processed_input.replace("\\{", "\\lbrace").replace("\\}", "\\rbrace").replace("<", "< ")
+    return processed_input
+}
+
 debug = require("./config").debug;
 module.exports = function (data) {
     console.log("Katex patch loaded. Made by BlitherBoom812.");
-    if (debug) {
-        console.log("before replace: " + data.content.match(/\$\$(.|\n)*?\$\$/g));
-    }
     data.content = data.content.replace(/\$(([^\n]| )+?)\$/g, (match, p1, offset, string) => {
-        if (debug){
-            console.log("matched content: ", p1)
-        }
-        return `<div style="display: inline;">\$${p1}\$</div>`
+        return `$${process_katex_formula(p1).replace("_", "\\_")}$`
     });
-    data.content = data.content.replace(/\$\$((.|\n|\s)*?)\$\$/g, '<div style="display: inline;">$$$$$1$$$$<\/div>');
-    if (debug) {
-        console.log("after replace: " + data.content.match(/<div>\$\$(.|\n)*?\$\$<\/div>/g));
-    }
-    data.content = data.content.replace("\\{", "\\lbrace");
-    data.content = data.content.replace("\\}", "\\rbrace");
+    data.content = data.content.replace(/\$\$((.|\n)*?)\$\$/g, (match, p1, offset, string) => {
+        console.log("matched $$: ", p1)
+        replaced_content = `<div>$$ ${process_katex_formula(p1)} $$</div>`
+        console.log("replaced_content: ", replaced_content)
+        return replaced_content
+    });
     return data;
 };
