21eaf8bc9ea1c355cf4b8eca414182154c3feac9
Guoyun Tian <guoyun812@qq.com>
2025-12-24 22:54:45 +0800

    perfect katex patch!

--- scripts/katex-patch.js
+++ scripts/katex-patch.js
@@ -3,26 +3,31 @@ debug = require("./config").debug;
 function process_katex_formula(input) {
     processed_input = input
     processed_input = processed_input
-        .replaceAll("\\{", "\\lbrace ")
-        .replaceAll("\\}", "\\rbrace ") //没这个会报错
-        .replaceAll("\\|", "\\Vert ") // 防止斜杠被干掉
-        .replaceAll("<", "< ") // 防止转义成 html 标签
+        .replaceAll(/</g, '&lt;')
+        .replaceAll(/>/g, '&gt;');
     return processed_input
 }
 module.exports = function (data) {
     console.log("Katex patch loaded. Made by BlitherBoom812.");
     console.log(data.content)
-    data.content = data.content.replace(/\$([^\n$]+)\$/g, (match, p1) => {
-        console.log("matched: ", match)
-        return_value = process_katex_formula(p1)
-            .replaceAll(/_/g, "\\_") // 防止被解析成 markdown 的下划线
-            .replaceAll(/\*/g, "\\\*") // 防止被解析成 markdown 的下划线
-            .replaceAll("\\\\", "\\\\\\\\") // 防止被解析成 markdown 的转义符
-        console.log("revised: ", return_value)
-        return `$${return_value}$`
+    data.content = data.content.replace(/(?<!\$)\$([^\n$]+)\$(?!\$)/g, (match, p1) => {
+        console.log("matched single line formula: ", match)
+        return_value = `{% raw %} $${process_katex_formula(p1)}$ {% endraw %}`
+        console.log("revised single line formula: ", return_value)
+        return return_value
     });
-    data.content = data.content.replace(/\$\$([\s\S]*?)\$\$/g, (match, p1) => {
-        return `<div>${process_katex_formula(match)}</div>`
+    data.content = data.content.replace(/\$\$([^$]+?)\$\$/g, (match, p1) => {
+        console.log("matched multiline formula: ", match)
+        return_value = `{% raw %}\n$$${process_katex_formula(p1)}$$\n{% endraw %}`
+        max_str_length = 800
+        if (return_value.length > max_str_length) {
+            console.log("revised (preview): ", 
+                `${return_value.slice(0, max_str_length / 2)} ... ${return_value.slice(-max_str_length / 2)}`
+            );
+        } else {
+            console.log("revised multiline formula: ", return_value);
+        }
+        return return_value        
     });
     return data;
 };
